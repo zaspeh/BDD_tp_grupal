c395aa37f6b0d2a01a413538f9d8c32537832a9e
ValFran99 <francovalfre999@gmail.com>
2024-12-03 14:11:57 -0300

    feat: now the backend and frontend can communicate between each other

--- frontend/src/Components/Pages/ShowDbs.jsx
+++ frontend/src/Components/Pages/ShowDbs.jsx
@@ -1,6 +1,28 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+
 
 export const ShowDbs = () => {
+
+  const [sqlData, setSqlData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/sql/getAll', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(response => response.json())
+    .then(data => {
+      console.log("Response: ");
+      console.log(data);
+      setSqlData(data);
+    }).catch(error => {
+      console.log("Error: ");
+      console.log(error);
+    })
+  }, []);
+
   return (
     <div className='h-full border border-red-500'>
       <h1>This should show both databases together</h1>
