658cdbe2d3340d2fad2b093a015937c1c9d36138
ValFran99 <francovalfre999@gmail.com>
2024-12-04 22:05:14 -0300

    fix: unignored the .env file from the .gitignore, this shouldn't be done on a real project

--- frontend/src/Components/Pages/ShowDbs.jsx
+++ frontend/src/Components/Pages/ShowDbs.jsx
@@ -24,8 +24,23 @@ export const ShowDbs = () => {
   }, []);
 
   return (
-    <div className='h-full border border-red-500'>
-      <h1>This should show both databases together</h1>
+    <div className='h-full border border-red-500 flex justify-items-center'>
+      <div className='border border-red-500'>
+        <h1>Mongo Database</h1>
+        {/* <ul>
+          {mongoData.map((item) => {
+            return <li key={item._id}>{item.name}</li>
+          })}
+        </ul> */}
+      </div>
+      <div className='border border-red-500'>
+        <h1>SQL Database</h1>
+        <ul>
+          {sqlData.map((item) => {
+            return <li key={item.id}>{item.name}</li>
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
