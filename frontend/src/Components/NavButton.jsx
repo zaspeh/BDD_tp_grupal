6b66b2b7e329d257b7c36282cd7d7fd93e9ce502
ValFran99 <francovalfre999@gmail.com>
2024-12-05 23:11:38 -0300

    feat: last graphic touches to the frontend

--- frontend/src/Components/NavButton.jsx
+++ frontend/src/Components/NavButton.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export const NavButton = ({buttonText, linkTo}) => {
   return (
 
-    <Link to={linkTo} className="h-10 w-40 bg-buttonBlue text-white border border-white-500 py-2 px-5 text-center inline-block text-base my-2 mx-1 cursor-pointer rounded-lg">
+    <Link to={linkTo} className="h-fit w-fit bg-buttonBlue text-white border border-white-500 py-2 px-5 text-center inline-block text-base my-2 mx-1 cursor-pointer rounded-lg">
       <li className="list-none">
       {buttonText}
       </li>
--- frontend/src/Components/Pages/Layout.jsx
+++ frontend/src/Components/Pages/Layout.jsx
@@ -1,14 +1,16 @@
 import { Outlet, Link } from "react-router-dom";
 import { NavButton } from "../NavButton";
+import { FaDatabase } from "react-icons/fa";
+
 
 export const Layout = () => {
   return (
     <div className="flex flex-col h-full">
       <nav className="w-full flex rounded-full border justify-between pl-3 pr-5 items-center bg-headerBlack mb-3">
-        <h1>NavBar title/logo/something</h1>
+        <FaDatabase className="ml-4" size={30}/>
         <ul className="flex">
-          <NavButton buttonText={"Add sql form"} linkTo={"/sqlForm"}/>
-          <NavButton buttonText={"Add noSql form"} linkTo={"/noSqlForm"}/>
+          <NavButton buttonText={"PostgreSql Entry Form"} linkTo={"/sqlForm"}/>
+          <NavButton buttonText={"MongoDB Entry Form"} linkTo={"/noSqlForm"}/>
           <NavButton buttonText={"Show databases"} linkTo={"/showDb"}/>
         </ul>
       </nav>
